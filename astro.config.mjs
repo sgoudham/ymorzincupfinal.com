64c5ebbb107cea62ba4d131a49fa337b79c7e3c9
sgoudham <sgoudham@gmail.com>
2026-04-26 00:14:26 +0100

    feat: something other than the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ymorzincupfinal.com",
-  integrations: [react()],
+	site: "https://ymorzincupfinal.com",
+	integrations: [react()],
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Alfa Slab One",
+			cssVariable: "--font-alfa-slab-one",
+		},
+	],
 });
